e09c26102afb9ca0b9ef3857aac358e031037d3b
rsundar <rohan.s.1987@gmail.com>
2019-09-24 17:28:22 +0530

    Added the bubble sort method and associated tests

--- lib/main.rb
+++ lib/main.rb
@@ -1,12 +1,26 @@
 def bubble_sort(array)
-    skip = -1
-    while skip != 0
-        for i in 0...(array.length-2) do
+    swap = true
+    while swap do
+        swap = false
+        for i in 0...(array.length-1) do
             left = array[i]
             right = array[i+1]
-            array[i],array[i+1] = array[i+1],array[i] if left>right
-            skip += 1
+            if left>right
+                array[i],array[i+1] = array[i+1],array[i]
+                swap=true
+            end 
         end
     end
     array
 end
+
+
+#test on integers
+arr = [12,3,54,1,2]
+puts bubble_sort(arr)
+#expected output 1,2,3,12,54
+puts "\n"
+#test on strings
+arr = ['bull','ball','zebra','goat','cat']
+puts bubble_sort(arr)
+#expected output ball,bull,cat,goat,zebra
---
+++ spec/test.rb
@@ -0,0 +1,7 @@
+#require '../lib/main.rb'
+
+#array = [23,45,12,55,109,2000,1,56]
+
+#output = bubble_sort(array)
+
+#puts "#{output}"
